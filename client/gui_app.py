63f059183bc0250fb85cb30fa6e952d0578ae8ec
Lusandre <lusandremarcano@gmail.com>
2022-02-28 16:55:30 -0400

    implementaciones de sqlite

--- client/gui_app.py
+++ client/gui_app.py
@@ -1,7 +1,8 @@
 import tkinter as tk 
 from tkinter import ttk
 import sqlite3
-
+from tkinter import messagebox as mb
+from datetime import datetime
 
 class Frame(tk.Frame):
 
@@ -53,13 +54,35 @@ class Frame(tk.Frame):
 		self.clave_line.place(x=20,y=235)
 
 		self.bonton_inicio = tk.PhotoImage(file="Imagen1.png")
-		self.btn_inicio = tk.Button(self, command = self.menu)
+		self.btn_inicio = tk.Button(self, command = self.login)
 		self.btn_inicio.config( image = self.bonton_inicio, bg = "DeepSkyBlue4", activebackground = "DeepSkyBlue4", cursor = 'hand2', border = 0)
 		self.btn_inicio.place(x="40",y="285")
 
+	def login(self):				#Funcion login ... Nos permitira comprobar 'usuario' y 'contraseña' con la base de datos
+		print('hi')
+		usuario=self.usuario_entry.get()		#Obtenemos el valor de la 'caja1' (usuario)
+		contr=self.clave_entry.get()		#Obtenemos el valor de la 'caja2' (contraseña)
+		print('hi')
+		query = 'SELECT * FROM usuario WHERE username = ? AND llave = ?'
+		
+		db_rows = self.run_query(query,(usuario,contr))
+
+		if db_rows.fetchall():
+			mb.showinfo(title="Login Correcto",message="Usuario y contraseña correctos")		#Mostramos 'Login Correcto'
+			self.menu()
+		else:
+			mb.showerror(title="Login incorrecto",message="Usuario o contraseña incorrecto")	#Mostramos 'Login incorrecto'
+		#c.close()
+
 	def vacio(self):
-		self.clave_string.set('')
-		self.usuario_string.set('')
+		self.textci.set('')
+		self.textnom.set('')
+		self.textape.set('')
+		self.textdir.set('')
+		self.texttlf.set('')
+		self.textmail.set('')
+		self.texteva.set('')
+		self.textante.set('')
 	
 	def clearFrame(self):
     # destroy all widgets from frame
@@ -180,7 +203,7 @@ class Frame(tk.Frame):
 		self.ci_line.place(x=20,y=95)
 
 		self.bonton_buscar = tk.PhotoImage(file="buscar.png")
-		self.bnt_salir= tk.Button(tabla,image=self.bonton_buscar,bg="DeepSkyBlue4", activebackground = "DeepSkyBlue4",border=0)
+		self.bnt_salir= tk.Button(tabla, command=self.buscar,image=self.bonton_buscar,bg="DeepSkyBlue4", activebackground = "DeepSkyBlue4",border=0)
 		self.bnt_salir.place(x=190,y=70)
 
 		self.tree = ttk.Treeview(tabla, height = 10, columns = ('','','','',''))
@@ -210,6 +233,24 @@ class Frame(tk.Frame):
 		self.bnt_volver.place(x=325,y=345)
 		self.get_products()
 
+	def buscar(self):
+		
+        # cleaning Table 
+		records = self.tree.get_children()
+		for element in records:
+			self.tree.delete(element)
+		ci=self.ci_entry.get()
+		print(ci)
+        # getting data
+		#query = 'SELECT * FROM paciente ORDER BY ci DESC'
+		query = 'SELECT fecha, ci, nombre, apellido, telefono FROM diagnostico, paciente WHERE diagnostico.cipaciente=paciente.ci AND diagnostico.cipaciente= ?'
+		
+		db_rows = self.run_query(query,(ci,))
+		# filling data
+		print (db_rows)
+		for row in db_rows:
+			self.tree.insert('', 0, values = (row[0],row[1],row[2], row[3],row[4]))
+
 	def registro(self):
 		global ventana_registro
 
@@ -224,76 +265,154 @@ class Frame(tk.Frame):
 
 		info = tk.Label(registro_frame,text="Registro de Nueva Historia Clinica",fg="white",bg="DeepSkyBlue4",font=("Bodoni MT",20,"bold")).place(x=10,y=0 )
 
-		nombre = tk.Label(registro_frame,text="Nombre",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
-		nombre.place(x=10,y=60)
-		nombre_entry = tk.Entry(registro_frame,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
-		nombre_entry.place(x=13,y=80)
-		nombre_line=tk.Frame(registro_frame,height=2,width=150)
-		nombre_line.place(x=13,y=105)
-
-		apellido = tk.Label(registro_frame,text="Apellido",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
-		apellido.place(x=170,y=60)
-		apellido_entry = tk.Entry(registro_frame,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
-		apellido_entry.place(x=170,y=80)
-		ape_line=tk.Frame(registro_frame,height=2,width=150)
-		ape_line.place(x=170,y=105)
+		self.textci = tk.StringVar()
+		self.textnom = tk.StringVar()
+		self.textape = tk.StringVar()
+		self.textdir = tk.StringVar()
+		self.textmail = tk.StringVar()
+		self.texttlf = tk.StringVar()
+		self.texteva = tk.StringVar()
+		self.textante = tk.StringVar()
 
 		cedula = tk.Label(registro_frame,text="Cedula",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
-		cedula.place(x=328,y=60)
-		cedula_entry = tk.Entry(registro_frame,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
-		cedula_entry.place(x=328,y=80)
+		cedula.place(x=10,y=60)
+		self.cedula_entry = tk.Entry(registro_frame, textvariable=self.textci, relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
+		self.cedula_entry.place(x=13,y=80)
 		ced_line=tk.Frame(registro_frame,height=2,width=150)
-		ced_line.place(x=328,y=105)
+		ced_line.place(x=13,y=105)
 
-		fecha = tk.Label(registro_frame,text="Fecha Dianostico",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
-		fecha.place(x=10,y=120)
-		fecha_entry = tk.Entry(registro_frame,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
-		fecha_entry.place(x=13,y=140)
-		fecha_line=tk.Frame(registro_frame,height=2,width=150)
-		fecha_line.place(x=13,y=165)
+		self.bnt_buscar = tk.PhotoImage(file="bus.png")
+		self.bnt_bus= tk.Button(registro_frame, command=self.bus,image=self.bnt_buscar,bg="DeepSkyBlue4", activebackground = "DeepSkyBlue4",border=0)
+		self.bnt_bus.place(x=135,y=80)
 
-		apellido = tk.Label(registro_frame,text="Correo",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
-		apellido.place(x=170,y=120)
-		apellido_entry = tk.Entry(registro_frame,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
-		apellido_entry.place(x=170,y=140)
+		
+		#Este texto aparecera en la entry por defecto:
+		#self.textnom.set('')
+
+		nombre = tk.Label(registro_frame,text="Nombre",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
+		nombre.place(x=170,y=60)
+		self.nombre_entry = tk.Entry(registro_frame, textvariable=self.textnom,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
+		self.nombre_entry.place(x=170,y=80)
+		nombre_line=tk.Frame(registro_frame,height=2,width=150)
+		nombre_line.place(x=170,y=105)
+
+		apellido = tk.Label(registro_frame,text="Apellido",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
+		apellido.place(x=328,y=60)
+		self.apellido_entry = tk.Entry(registro_frame, textvariable=self.textape,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
+		self.apellido_entry.place(x=328,y=80)
+		ape_line=tk.Frame(registro_frame,height=2,width=150)
+		ape_line.place(x=328,y=105)
+
+		direccion = tk.Label(registro_frame,text="Dirección",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
+		direccion.place(x=10,y=120)
+		self.direccion_entry = tk.Entry(registro_frame, textvariable=self.textdir,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
+		self.direccion_entry.place(x=13,y=140)
+		dir_line=tk.Frame(registro_frame,height=2,width=150)
+		dir_line.place(x=13,y=165)
+
+		'''cedula = tk.Label(registro_frame,text="Cedula",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
+										cedula.place(x=328,y=60)
+										cedula_entry = tk.Entry(registro_frame,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
+										cedula_entry.place(x=328,y=80)
+										ced_line=tk.Frame(registro_frame,height=2,width=150)
+										ced_line.place(x=328,y=105)
+								
+										fecha = tk.Label(registro_frame,text="Fecha Dianostico",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
+										fecha.place(x=10,y=120)
+										fecha_entry = tk.Entry(registro_frame,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
+										fecha_entry.place(x=13,y=140)
+										fecha_line=tk.Frame(registro_frame,height=2,width=150)
+										fecha_line.place(x=13,y=165)'''
+
+		correo = tk.Label(registro_frame,text="Correo",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
+		correo.place(x=170,y=120)
+		self.correo_entry = tk.Entry(registro_frame, textvariable=self.textmail,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
+		self.correo_entry.place(x=170,y=140)
 		ape_line=tk.Frame(registro_frame,height=2,width=150)
 		ape_line.place(x=170,y=165)
 
 		telefono = tk.Label(registro_frame,text="Telefono",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
 		telefono.place(x=328,y=120)
-		telefono_entry = tk.Entry(registro_frame,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
-		telefono_entry.place(x=328,y=140)
+		self.telefono_entry = tk.Entry(registro_frame, textvariable=self.texttlf,relief="flat",width=20,bg="DeepSkyBlue4",fg="white",font=("Arial",11,"bold")) 
+		self.telefono_entry.place(x=328,y=140)
 		tlf_line=tk.Frame(registro_frame,height=2,width=150)
 		tlf_line.place(x=328,y=165)
 
 		ante = tk.Label(registro_frame,text="Antecedentes del Paciente",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
 		ante.place(x=10,y=175)
-		ante_entry = tk.Entry(registro_frame,relief="flat",width=105,bg="white",fg="black",font=("Arial",11,"bold")) 
-		ante_entry.place(x=13,y=200)
+		self.ante_entry = tk.Entry(registro_frame, textvariable=self.textante,relief="flat",width=105,bg="white",fg="black",font=("Arial",11,"bold")) 
+		self.ante_entry.place(x=13,y=200)
 
 		eva = tk.Label(registro_frame,text="Evaluacion del Paciente",fg="white",bg="DeepSkyBlue4",font=("Arial",10,"bold")) 
 		eva.place(x=10,y=230)
-		eva_entry = tk.Entry(registro_frame,relief="flat",width=105,bg="white",fg="black",font=("Arial",11,"bold")) 
-		eva_entry.place(x=13,y=255)
+		self.eva_entry = tk.Entry(registro_frame, textvariable=self.texteva,relief="flat",width=105,bg="white",fg="black",font=("Arial",11,"bold")) 
+		self.eva_entry.place(x=13,y=255)
 
 		self.boton_registrar = tk.PhotoImage(file="BOTON1.png")
 		self.boton_cacelar =  tk.PhotoImage(file="BOTON2.png")
 
-		BTN_R = tk.Button(registro_frame,image=self.boton_registrar,bg="DeepSkyBlue4", activebackground = "DeepSkyBlue4",border=0)
+		BTN_R = tk.Button(registro_frame, command = self.registra,image=self.boton_registrar,bg="DeepSkyBlue4", activebackground = "DeepSkyBlue4",border=0)
 		BTN_R.place(x=70,y=300)
 
 		BTN_C = tk.Button(registro_frame,command = ventana_registro.destroy ,image=self.boton_cacelar,bg="DeepSkyBlue4", activebackground = "DeepSkyBlue4",border=0)
 		BTN_C.place(x=300,y=300)
 	
 	 # Get Products from Database
+
+	def bus(self):
+
+		ci=self.cedula_entry.get()
+		print(ci)
+        # getting data
+		#query = 'SELECT * FROM paciente ORDER BY ci DESC'
+		query = 'SELECT * FROM paciente WHERE ci= ?'
+		
+		db_rows = self.run_query(query,(ci,))
+		# filling data
+		print (db_rows)
+		for row in db_rows:
+			self.textnom.set(row[1])
+			self.textape.set(row[2])
+			self.textdir.set(row[3])
+			self.textmail.set(row[4])
+			self.texttlf.set(row[5])
+
+	def registra(self):
+		ci = self.cedula_entry.get()
+
+		query = 'SELECT * FROM paciente WHERE ci = ?'
+		
+		db = self.run_query(query,(ci,))
+		if not (db.fetchone()): 
+			self.reg_paciente()
+
+		self.reg_diagnostico()
+
+
+	def reg_diagnostico(self):
+		query = 'INSERT INTO diagnostico VALUES (?,?,?,?)'
+		date = str(datetime.today().strftime('%Y-%m-%d'))	
+		print(date)
+		parameters = (self.cedula_entry.get(), date, self.ante_entry.get(), self.eva_entry.get())
+		self.run_query(query,parameters)
+		self.vacio()
+
+	def reg_paciente(self):
+		query = 'INSERT INTO paciente VALUES (?,?,?,?,?,?,?)'
+		parameters = (self.cedula_entry.get(), self.nombre_entry.get(), self.apellido_entry.get(), self.direccion_entry.get(), self.correo_entry.get(), self.telefono_entry.get(),"28")
+		self.run_query(query,parameters)
+
 	def get_products(self):
         # cleaning Table 
 		records = self.tree.get_children()
 		for element in records:
 			self.tree.delete(element)
         # getting data
-		query = 'SELECT * FROM paciente ORDER BY ci DESC'
+		#query = 'SELECT * FROM paciente ORDER BY ci DESC'
+		query = 'SELECT fecha, ci, nombre, apellido, telefono FROM diagnostico, paciente WHERE diagnostico.cipaciente=paciente.ci ORDER BY fecha DESC'
+		
 		db_rows = self.run_query(query)
 		# filling data
+		print (db_rows)
 		for row in db_rows:
-			self.tree.insert('', 0, values = ('',row[0],row[1],row[2], row[5]))
+			self.tree.insert('', 0, values = (row[0],row[1],row[2], row[3],row[4]))
